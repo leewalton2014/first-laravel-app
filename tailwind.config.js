a4bb2bec7d97d12b3586e9a054f780c528ece4cb
Lee Walton <lee.walton2014@outlook.com>
2022-01-21 17:27:24 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        "./resources/views/layouts/app.blade.php",
+        "./resources/views/posts/index.blade.php",
+        "./resources/views/auth/register.blade.php",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
