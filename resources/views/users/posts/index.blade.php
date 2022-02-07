fab2906e3f3f7b6f2a5ac5602a9eae92d31ecbd3
Lee Walton <lee.walton2014@outlook.com>
2022-02-07 04:18:50 +0000

    follow button

--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -9,6 +9,22 @@
                 <div>
                     <h1 class="font-medium text-2xl">{{ $user->name }}</h1>
                     <span class="mb-2 text-gray-600">{{"@"}}{{ $user->username }}</span>
+
+                    @auth
+                    {{-- @if (!$post->likedBy(auth()->user())) --}}
+                        <form action="" method="post" class="mr-2">
+                            @csrf
+                            <button type="submit" class="mt-2 px-5 font-semibold rounded-md bg-blue-500 text-white p-2">Follow</button>
+                        </form>
+                    {{-- @else --}}
+                        <form action="" method="post" class="mr-2 mb-2">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="mt-2 px-5 font-semibold rounded-md bg-red-500 text-white p-2">Unfollow</button>
+                        </form>
+                    {{-- @endif --}}
+                    @endauth
+
                     <p>Posted {{ $posts->count() }} {{ Str::plural('Post', $posts->count()) }}</p>
                     <p>Received {{ $user->receivedLikes->count() }} Likes</p>
                 </div>
